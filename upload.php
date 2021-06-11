c583a0930b25b5925a05f7654c06615c6d8773da
adam nunez <adam.a.nunez@gmail.com>
2021-06-11 16:18:56 -0500

    Added more logging around file uploads server side

--- upload.php
+++ upload.php
@@ -5,10 +5,13 @@ if (isset($_FILES['file'])) {
         "tmp" => $_FILES['file']['tmp_name'],
         "target" => $_POST['destination']
     ];
+    error_log("File ".$_FILES['file']['tmp_name']." uploaded by Audio Recorder. Destination ".$_POST['destination'], 0);
     $dir = implode( DIRECTORY_SEPARATOR, array_slice(explode( DIRECTORY_SEPARATOR, $_POST['destination']), 0, -1));
     mkdir( $dir, 0777, true);
     if ( move_uploaded_file($_FILES['file']['tmp_name'], $_POST['destination'] ) )
         $out["success"] = true;
+    else
+        error_log("Error moving ".$_FILES['file']['tmp_name'], 0);
     echo json_encode($out);
 } else {
     echo json_encode([
